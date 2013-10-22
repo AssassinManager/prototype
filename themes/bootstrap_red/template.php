18cf0ae056bf6a270a94c26489e408dc5a91970c
Alex Jewell <alex@alexjewell.com>
2013-10-22 18:18:57 -0500

    Red Theme Updates

--- themes/bootstrap_red/template.php
+++ themes/bootstrap_red/template.php
@@ -1,5 +1,6 @@
 <?php
 
-/**
- * @file template.php
- */
+function mytheme_preprocess_html(&$variables) {
+  drupal_add_css('http://fonts.googleapis.com/css?family=PT+Sans+Narrow', array('type' => 'external'));
+  drupal_add_css('http://fonts.googleapis.com/css?family=PT+Sans', array('type' => 'external'));
+}
